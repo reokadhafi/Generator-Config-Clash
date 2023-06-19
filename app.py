2633e5210ac3950b4b8bb64e6fe31ed16987cdce
reokadhafi <106902195+reokadhafi@users.noreply.github.com>
2023-06-19 17:32:40 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+import webbrowser as wb
+from clash1 import write_database, add_akun, clear_akun, generate, write_config, result
+from flask import Flask, render_template, url_for, request, redirect
+from base64 import b64decode
+
+url = "http://127.0.0.1:5000"
+
+
+def runbrowser(urls):
+    wb.open(urls)
+
+
+application = Flask(__name__)
+
+# Function baru di bawah ini
+
+
+@application.route('/', methods=['GET', 'POST'])
+def index():
+    dbakun = "None"
+    mode = ""
+    if request.method == 'POST':
+        if request.form.get('save') == 'Save':
+            akunn = request.form['akun']
+            file_db = open(('db_save.txt'), "a+")
+            file_db.write(f"{akunn}\n")
+            file_db.close()
+            write_database(add_akun(akunn))
+        elif request.form.get('clear') == 'Clear Data':
+            file_db = open(('db_save.txt'), "w")
+            file_db.write("")
+            file_db.close()
+            write_database(clear_akun("vmess"))
+            write_database(clear_akun("trojan"))
+            write_database(clear_akun("ss"))
+        elif request.form.get('generate') == 'Generate':
+            # dbakun = write_config(generate(request.form["modemode"].split(",")[
+            #     0], request.form["modemode"].split(",")[1], request.form["modemode"].split(",")[2]))
+            write_config(generate(request.form["modemode"].split(",")[
+                0], request.form["modemode"].split(",")[1], request.form["modemode"].split(",")[2]))
+            dbakun = result()
+            mode = request.form["modemode"].split(",")
+        # return render_template('index.html', akunn=akunn, dbakun=dbakun)
+    return render_template('index.html', dbakun=dbakun, mode=mode)
+
+
+runbrowser(url)
+if __name__ == '__main__':
+    application.run(debug=True)
---
+++ clash1.py
@@ -0,0 +1,437 @@
+# %%
+import re
+import json
+import yaml
+from urllib.parse import urlparse, parse_qs
+from base64 import b64decode
+
+# kumpulan parser terpakai di function add_akun
+
+
+def parser_trojan(data):
+    parsed_url = urlparse("trojan://"+data)
+    query_params = parse_qs(parsed_url.query)
+    if parsed_url.scheme != "trojan" or not parsed_url.netloc:
+        print("Data akun Trojan tidak valid!")
+        return
+    password = parsed_url.netloc.split("@")[0]
+    server = parsed_url.netloc.split("@")[1].split(":")[0]
+    port = parsed_url.port
+    sni = query_params.get("sni", [""])[0]
+    tipe = query_params.get("type", [""])[0]
+    host = query_params.get("host", [""])[0]
+    path = query_params.get("path", [""])[0]
+    data_dict = {
+        "password": password,
+        "server": server,
+        "port": port,
+        "sni": sni,
+        "type": tipe,
+        "host": host,
+        "path": path
+    }
+    return data_dict
+
+
+def parser_v2ray(data):
+    dd = b64decode(data + '=' * (-len(data) % 4)).decode()
+    rl = (dd.replace(" ", "").replace("\r", "").replace(
+        "\n", "")).replace('"', "'")[1:-1].replace("GET-:wss://", "formatbaru//").replace("http://", "http=//")
+    db1 = {
+        'add': '',
+        'aid': '',
+        'host': '',
+        'id': '',
+        'net': '',
+        'path': '',
+        'port': '',
+        'ps': '',
+        'scy': '',
+        'sni': '',
+        'tls': '',
+        'type': '',
+        'v': ''
+    }
+    for i in range(len(rl.split(':'))-1):
+        dt = rl.split(",")[i].replace("'", '').split(":")
+        db1[dt[0]] = dt[1]
+        print(dt)
+    return db1
+
+
+def parser_ss(data):
+    db1 = {
+        'password': '',
+        'server': '',
+        'port': '80',
+        'udp-over-tcp': 'true',
+        'plugin': 'v2ray-plugin',
+        'path': '',
+    }
+    rl = (data.replace(" ", "").replace(
+        "/", "").replace("%2F", "/").replace("%3B", "&").replace("%3D", "=").replace("#", "&name="))
+    dd = b64decode(rl.split("@")[0] + '=' *
+                   (-len(rl.split("@")[0]) % 4)).decode()
+    db1['password'] = dd.split(":")[1]
+    db1['cipher'] = dd.split(":")[0]
+    db1['server'] = rl.split('@')[1].split(':')[0]
+    dt = rl.split('?')[1].split("&")
+    for i in range(len(dt)):
+        dt2 = dt[i].split("=")
+        db1[dt2[0]] = dt2[1]
+    return db1
+
+# menulis ke file database
+
+
+def write_database(add_or_clear_akun, filename="log.json"):
+    with open(filename, "w") as db:
+        json.dump(add_or_clear_akun, db)
+
+
+def add_akun(string_akun):
+    with open("log.json") as json_files:
+        data = json.load(json_files)
+        akun_mentah = data["akun_mentah"]
+        akun_parsing = data["akun_parsing"]
+        db = {}
+        if string_akun[0:8] == 'vmess://':
+            db = string_akun.replace('vmess://', '')
+            akun_mentah["vmess"].append(db)
+            akun_parsing["vmess"].append(
+                parser_v2ray(db))
+        elif string_akun[0:8] == 'trojan:/':
+            db = string_akun.replace('trojan://', '')
+            akun_mentah["trojan"].append(db)
+            akun_parsing["trojan"].append(parser_trojan(db))
+        elif string_akun[0:8] == 'trojan-g':
+            db = string_akun.replace('trojan-go://', '')
+            akun_mentah["trojan"].append(db)
+            akun_parsing["trojan"].append(parser_trojan(db))
+        elif string_akun[0:5] == 'ss://':
+            db = string_akun.replace('ss://', '')
+            akun_mentah["ss"].append(db)
+            akun_parsing["ss"].append(parser_trojan(db))
+        return data
+
+# untuk menghapus data akun sesuai jenis (vmess,trojan,ss)
+
+
+def clear_akun(jenis_akun):
+    with open("log.json") as json_files:
+        data = json.load(json_files)
+        akun_mentah = data["akun_mentah"]
+        akun_parsing = data["akun_parsing"]
+    if jenis_akun == "vmess":
+        akun_mentah["vmess"].clear()
+        akun_parsing["vmess"].clear()
+    if jenis_akun == "trojan":
+        akun_mentah["trojan"].clear()
+        akun_parsing["trojan"].clear()
+    if jenis_akun == "ss":
+        akun_mentah["ss"].clear()
+        akun_parsing["ss"].clear()
+    return data
+# write_database(add_akun(data))
+# write_database(clear_akun("vmess"))
+
+# %%
+# mencari nama server sesuai data server
+
+
+def nama_server(server):
+    with open("db_server.json") as json_file:
+        data = json.load(json_file)
+    for i, bc in data["data_server"].items():
+        for ii in range(len(bc)):
+            if server in bc[ii]:
+                return bc[ii][server], i
+
+# %%
+# untuk generate dari database
+
+
+def generate(jenis, mode, tipe):
+    db = []
+    with open("log.json") as json_files:
+        data = json.load(json_files)
+        akun_parsing = data["akun_parsing"]
+        data_bug = data["data_bug"]
+        if jenis == "vmess":
+            read_db = akun_parsing["vmess"]
+            for i in range(len(read_db)):
+                cm = read_db[i]
+                dt = {}
+                if mode == "gamemax":
+                    bug = "cf-vod.nimo.tv"
+                    if tipe == "lama":
+                        search = [cm['add'], cm['sni'], cm['ps'], cm['host']]
+                        for i in range(len(search)):
+                            try:
+                                if nama_server(search[i]) is not None and search[i] != nama_server(search[i])[0]:
+                                    dd = nama_server(search[i])[0]
+                                    db_server = search[i]
+                                else:
+                                    #dd = "NO_DATA_SERVER"
+                                    for i in range(len(search)):
+                                        if "." in search[i] and search[i] != bug:
+                                            db_server = f"{search[i]}"
+                                            dd = db_server+"-None_Db"
+                            except TypeError:
+                                continue
+                            dt['name'] = f"{dd}-gm"
+                        dt['server'] = bug
+                        dt['port'] = 8080
+                        dt['type'] = 'vmess'
+                        dt['uuid'] = cm['id']
+                        dt['alterId'] = 0
+                        dt['cipher'] = 'auto'
+                        dt['tls'] = 'false'
+                        dt['skip-cert-verify'] = 'true'
+                        cm['sni'] = db_server
+                        dt['servername'] = cm['sni']
+                        dt['network'] = 'ws'
+                        if cm["host"] in data_bug:
+                            cm["host"] = db_server
+                        dt['ws-opts'] = {'path': cm['path'],
+                                         'headers': {'Host': db_server}}
+                        dt['udp'] = 'true'
+                    if tipe == "baru":
+                        search = [cm['add'], cm['sni'], cm['ps'], cm['host']]
+                        for i in range(len(search)):
+                            try:
+                                if nama_server(search[i]) is not None and search[i] != nama_server(search[i])[0]:
+                                    dd = nama_server(search[i])[0]
+                                    db_server = search[i]
+                                else:
+                                    #dd = "NO_DATA_SERVER"
+                                    for i in range(len(search)):
+                                        if "." in search[i] and search[i] != bug:
+                                            db_server = f"{search[i]}"
+                                            dd = db_server+"-None_Db"
+                            except TypeError:
+                                continue
+                            dt['name'] = f"{dd}-gmb"
+                        dt['server'] = bug
+                        dt['port'] = 443
+                        dt['type'] = 'vmess'
+                        dt['uuid'] = cm['id']
+                        dt['alterId'] = 0
+                        dt['cipher'] = 'auto'
+                        dt['tls'] = 'true'
+                        dt['skip-cert-verify'] = 'true'
+                        dt['servername'] = bug
+                        dt['network'] = 'ws'
+                        cm["host"] = db_server
+                        dt['ws-opts'] = {'path': f'GET-:wss://{bug}/fastssh/',
+                                         'headers': {'Host': cm['host']}}
+                        dt['udp'] = 'true'
+                if mode == "opok":
+                    bug = "corona.jakarta.go.id"
+                    if tipe == "lama":
+                        search = [cm['add'], cm['sni'], cm['ps'], cm['host']]
+                        for i in range(len(search)):
+                            try:
+                                if nama_server(search[i]) is not None and search[i] != nama_server(search[i])[0]:
+                                    dd = nama_server(search[i])[0]
+                                    db_server = search[i]
+                                else:
+                                    #dd = "NO_DATA_SERVER"
+                                    for i in range(len(search)):
+                                        if "." in search[i] and search[i] != bug:
+                                            db_server = f"{search[i]}"
+                                            dd = db_server+"-None_Db"
+                            except TypeError:
+                                continue
+                            dt['name'] = f"{dd}-op"
+                        dt['server'] = bug
+                        dt['port'] = 443
+                        dt['type'] = 'vmess'
+                        dt['uuid'] = cm['id']
+                        dt['alterId'] = 0
+                        dt['cipher'] = 'auto'
+                        dt['tls'] = 'true'
+                        dt['skip-cert-verify'] = 'true'
+                        cm['sni'] = bug
+                        dt['servername'] = cm['sni']
+                        dt['network'] = 'ws'
+                        if cm["host"] in data_bug:
+                            cm["host"] = db_server
+                        dt['ws-opts'] = {'path': f"wss://{bug}{cm['path']}",
+                                         'headers': {'Host': cm['host']}}
+                        dt['udp'] = 'true'
+                    if tipe == "baru":
+                        search = [cm['add'], cm['sni'], cm['ps'], cm['host']]
+                        for i in range(len(search)):
+                            try:
+                                if nama_server(search[i]) is not None and search[i] != nama_server(search[i])[0]:
+                                    dd = nama_server(search[i])[0]
+                                    db_server = search[i]
+                                else:
+                                    #dd = "NO_DATA_SERVER"
+                                    for i in range(len(search)):
+                                        if "." in search[i] and search[i] != bug:
+                                            db_server = f"{search[i]}"
+                                            dd = db_server+"-None_Db"
+                            except TypeError:
+                                continue
+                            dt['name'] = f"{dd}-opb"
+                        dt['server'] = bug
+                        dt['port'] = 443
+                        dt['type'] = 'vmess'
+                        dt['uuid'] = cm['id']
+                        dt['alterId'] = 0
+                        dt['cipher'] = 'auto'
+                        dt['tls'] = 'true'
+                        dt['skip-cert-verify'] = 'true'
+                        dt['servername'] = bug
+                        dt['network'] = 'ws'
+                        cm["host"] = db_server
+                        dt['ws-opts'] = {'path': f'GET-:wss://{bug}/fastssh/',
+                                         'headers': {'Host': cm['host']}}
+                        dt['udp'] = 'true'
+                    if tipe == "worry":
+                        search = [cm['add'], cm['sni'], cm['ps'], cm['host']]
+                        for i in range(len(search)):
+                            try:
+                                if nama_server(search[i]) is not None and search[i] != nama_server(search[i])[0]:
+                                    dd = nama_server(search[i])[0]
+                                    db_server = search[i]
+                                else:
+                                    #dd = "NO_DATA_SERVER"
+                                    for i in range(len(search)):
+                                        if "." in search[i] and search[i] != bug:
+                                            db_server = f"{search[i]}"
+                                            dd = db_server+"-None_Db"
+                            except TypeError:
+                                continue
+                            dt['name'] = f"{dd}-opw"
+                        dt['server'] = db_server
+                        dt['port'] = 80
+                        dt['type'] = 'vmess'
+                        dt['uuid'] = cm['id']
+                        dt['alterId'] = 0
+                        dt['cipher'] = 'auto'
+                        dt['tls'] = 'false'
+                        dt['skip-cert-verify'] = 'false'
+                        dt['servername'] = "tsel.me"
+                        dt['network'] = 'ws'
+                        if cm["host"] in data_bug:
+                            cm["host"] = data_bug
+                        dt['ws-opts'] = {'path': "/worryfree",
+                                         'headers': {'Host': "tsel.me"}}
+                        dt['udp'] = 'true'
+                db.append(dt)
+        if jenis == "trojan":
+            read_db = akun_parsing["trojan"]
+            for i in range(len(read_db)):
+                cm = read_db[i]
+                dt = {}
+                if mode == "gamemax":
+                    bug = "cf-vod.nimo.tv"
+                    if tipe == "lama":
+                        if nama_server(cm['host']) is not None:
+                            dt['name'] = f"{nama_server(cm['host'])[0]}-gm"
+                        else:
+                            dt['name'] = f"{cm['host']}-None_Db-gm"
+                        dt['server'] = bug
+                        dt['port'] = 443
+                        dt['type'] = 'trojan'
+                        dt['password'] = cm['password']
+                        dt['tls'] = 'true'
+                        dt['skip-cert-verify'] = 'true'
+                        dt['sni'] = cm['host']
+                        dt['network'] = 'ws'
+                        dt['ws-opts'] = {'path': cm['path'],
+                                         'headers': {'Host': cm['host']}}
+                        dt['udp'] = 'true'
+                if mode == "opok":
+                    bug = "corona.jakarta.go.id"
+                    if tipe == "lama":
+                        if nama_server(cm['host']) is not None:
+                            dt['name'] = f"{nama_server(cm['host'])[0]}-op"
+                        else:
+                            dt['name'] = f"{cm['host']}-None_Db-op"
+                        dt['server'] = bug
+                        dt['port'] = 443
+                        dt['type'] = 'trojan'
+                        dt['password'] = cm['password']
+                        dt['tls'] = 'true'
+                        dt['skip-cert-verify'] = 'true'
+                        dt['sni'] = cm['host']
+                        dt['network'] = 'ws'
+                        dt['ws-opts'] = {'path': cm['path'],
+                                         'headers': {'Host': cm['host']}}
+                        dt['udp'] = 'true'
+                db.append(dt)
+        if jenis == "ss":
+            read_db = akun_parsing["ss"]
+            for i in range(len(read_db)):
+                cm = read_db[i]
+                dt = {}
+                if mode == "gamemax":
+                    bug = "cf-vod.nimo.tv"
+                    if tipe == "lama":
+                        if nama_server(cm['server']) is not None:
+                            dt['name'] = f"{nama_server(cm['server'])[0]}-gm"
+                        else:
+                            dt['name'] = f"{cm['password']}-None_Db-gm"
+                        dt['server'] = bug
+                        dt['port'] = 80
+                        dt['type'] = 'ss'
+                        dt['cipher'] = cm['cipher']
+                        dt['password'] = f"'{cm['password']}'"
+                        dt['udp-over-tcp'] = 'true'
+                        dt['plugin'] = 'v2ray-plugin'
+                        dt['plugin-opts'] = {'mode': 'websocket', 'host': cm['server'],
+                                             'path': cm['path'], 'tls': 'false', 'skip-cert-verify': 'true', 'mux': 'true'}
+                if mode == "opok":
+                    bug = "corona.jakarta.go.id"
+                    if tipe == "lama":
+                        if nama_server(cm['server']) is not None:
+                            dt['name'] = f"{nama_server(cm['server'])[0]}-op"
+                        else:
+                            dt['name'] = f"{cm['password']}-None_Db-op"
+                        dt['server'] = bug
+                        dt['port'] = 443
+                        dt['type'] = 'ss'
+                        dt['cipher'] = cm['cipher']
+                        dt['password'] = f"'{cm['password']}'"
+                        dt['udp-over-tcp'] = 'true'
+                        dt['plugin'] = 'v2ray-plugin'
+                        dt['plugin-opts'] = {
+                            'mode': 'websocket',
+                            'host': cm['server'],
+                            'path': f"wss://{bug}{cm['path']}",
+                            'tls': 'true',
+                            'skip-cert-verify': 'true',
+                            'mux': 'true',
+                            'sni': bug
+                        }
+                db.append(dt)
+    return db
+
+# %%
+# read_generate = generate("vmess", "gamemax", "lama")
+# write_config untuk menulis config.yaml yang sudah di generate
+
+
+def write_config(read_generate):
+    with open('config.yaml', 'w') as yaml_file:
+        yaml.dump([{"proxies": read_generate}],
+                  yaml_file, sort_keys=False)
+    with open('config.yaml', 'r') as yaml_file2:
+        update = yaml_file2.read().replace(
+            "'true'", "true").replace("- proxies:", "proxies:").replace("'false'", "false")
+        with open('config.yaml', 'w') as yaml_file3:
+            yaml_file3.write(update)
+
+# result untuk menampilkan hasil akhir dari config.yaml
+
+
+def result():
+    with open('config.yaml', 'r') as yaml_file:
+        cetak = yaml_file.read()
+        print(cetak)
+        return cetak
+# write_config(read_generate)
